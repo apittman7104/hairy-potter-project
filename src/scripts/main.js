5172b238e98543311e8e1e9ffe2b0d9b5b025a5e
Aedin Pittman <aedinpittman@gmail.com>
2023-07-26 10:23:39 -0500

    Displaying objects in browser

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,7 @@
 import { makePottery } from "./PotteryWheel.js"
 import { firePottery } from "./Kiln.js";
 import { usePottery, toSellOrNotToSell } from "./PotteryCatalog.js";
+import { PotteryList } from "./PotteryList.js";
 
 // Make 5 pieces of pottery at the wheel
 let mug = makePottery("Mug", 4, 6);
@@ -26,6 +27,5 @@ toSellOrNotToSell(firedPlate);
 console.log(usePottery());
 
 // Invoke the component function that renders the HTML list
-
-
-
+const parentTag = document.querySelector(".potteryList");
+parentTag.innerHTML = PotteryList(usePottery());
